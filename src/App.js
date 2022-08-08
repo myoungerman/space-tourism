e73e2cb1f963994b4a89c25d8fc0035cb0482ff4
unknown <mcyoungerman@gmail.com>
2022-08-08 11:06:02 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="heading--5">title</p>
+      <p>text</p>
+    </div>
+  );
+}
+
+export default App;
