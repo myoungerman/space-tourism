b0cda582bd34d584cff975f9821f66981c50a046
unknown <mcyoungerman@gmail.com>
2022-08-11 09:32:38 -0700

    Create tablet layout for intro screen

--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -4,11 +4,10 @@ import styles from './IntroScreen.module.css';
 export default function IntroScreen() {
     return (
         <div className={styles.IntroScreen}>
-            {/* <img src="https://svgshare.com/i/ju3.svg" alt='Earth' className={`${styles['IntroScreen__img']} ${styles['img--background']}`}></img> */}
             <div className={`${styles['text-container']} ${styles['IntroScreen__text-div']}`}>
                 <p className={`heading--5 blue ${styles['IntroScreen__text--no-top-and-bottom-margin']}`}>So, you want to travel to</p>
                 <p className={`heading--1 ${styles['IntroScreen__large-word']}`}>Space</p>
-                <p className={styles['IntroScreen__text--no-top-and-bottom-margin']}>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+                <p className={`blue ${styles['IntroScreen__text--no-top-and-bottom-margin']}`}>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
             </div>
             <div className={`${styles.circle} ${styles['IntroScreen__button']}`}></div>
             <div className={`${styles['IntroScreen__button-container']}`}>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar(props) {
             <img src="https://svgshare.com/i/jtu.svg" className={`${styles.star} ${styles['NavBar__star']}`} alt="Star icon"></img>
             <div className={styles.line}></div>
             <nav className={styles['nav--main']}>
-                <a><span className='bold active'>00</span> HOME</a>
+                <a><span className='bold'>00</span> HOME</a>
                 <a><span className='bold'>01</span> DESTINATION</a>
                 <a><span className='bold'>02</span> CREW</a>
                 <a><span className='bold'>03</span> TECHNOLOGY</a>
